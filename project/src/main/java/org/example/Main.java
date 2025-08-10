167b279de8cdf776ac6cba63697ac164fbb675f7
Arvuud <st192413@stud.uni-stuttgart.de>
2025-08-10 14:08:39 +0200

    added

--- project/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        final NeurAInc neurAInc = new NeurAInc();
-        AIClassifier aiClassifier = new AIClassifier();
-        System.out.println(aiClassifier.classify("Hey, buddy"));
-    }
-}
